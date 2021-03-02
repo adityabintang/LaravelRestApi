c739c24d2d51ef52a196d55280ae7809c74cb054
Aditya Bintang F <45384894+adityabintang@users.noreply.github.com>
2021-03-02 16:40:18 +0700

    Membuat route untuk delete

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::any('editSiswa/{id}', "App\Http\Controllers\ApiSiswa@editSiswa");
 Route::any('tambahUser', "App\Http\Controllers\UserController@tambahUser");
 Route::any('updateUser/{id}', "App\Http\Controllers\UserController@updateUser");
 Route::any('getUser', "App\Http\Controllers\UserController@getUser");
-
+Route::any('deleteUser', "App\Http\Controllers\UserController@deleteUser");
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $requany>user();
 });
